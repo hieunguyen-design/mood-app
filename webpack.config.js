e24273b55dc976af828e8f32eddfb4527cc1b1e2
hieunguyen-design <htnguyen1310@gmail.com>
2025-09-05 14:26:10 -0400

    Initial webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js", // Entry point
@@ -13,16 +14,23 @@ module.exports = {
       template: "./src/template.html", // Handle HTML
     }),
   ],
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    static: "./dist", // Serve content from /dist
+    open: true, // Auto-open browser
+    hot: true, // Enable hot module replacement (if applicable)
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"], // Handle CSS
       },
-      //      {
-      //			  test: /\.html$/i,
-      //			  loader: "html-loader", //html-loader for Image
-      //			},
+      //   {
+      //     test: /\.html$/i,
+      //     loader: "html-loader", //html-loader for Image
+      //   },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i, //Image loader
         type: "asset/resource",
